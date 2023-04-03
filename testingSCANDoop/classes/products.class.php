5730ddae7dbec81e068f47dc85a2e294e40dc3d0
Yashar Baradarvar <83546651+DavidYashar@users.noreply.github.com>
2023-04-03 10:47:28 +0300

    Add files via upload

---
+++ testingSCANDoop/classes/products.class.php
@@ -0,0 +1,17 @@
+<?php
+// interface Save {
+//     public function save($na, $de, $pr);
+// }
+include_once 'include/autoloader.inc.php';
+abstract class Products{
+// abstract protected function save($sku, $n, $p, $w, $h, $l, $s, $W);
+
+
+abstract function submit();
+    
+
+
+}
+
+
+?>
